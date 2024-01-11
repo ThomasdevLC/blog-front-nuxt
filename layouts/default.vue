41fbfcb89302bc2c6f2cd89ec96c487dc374b6a2
ThomasdevLC <thomas.lecam@gmail.com>
2024-01-11 16:48:49 +0100

    layouts header

---
+++ layouts/default.vue
@@ -0,0 +1,22 @@
+<template>
+  <header class="px-24 py-4 bg-black text-white">
+    <div class="flex justify-between items-center">
+      <p class="text-3xl">Amitiés Taléa</p>
+      <div class="flex gap-10 text-lg">
+        <NuxtLink to="/">Accueil</NuxtLink>
+        <NuxtLink to="/infos">Infos</NuxtLink>
+        <NuxtLink to="/events">Événements</NuxtLink>
+        <NuxtLink to="/news">Actualités</NuxtLink>
+      </div>
+    </div>
+  </header>
+  <div>
+    <slot />
+  </div>
+</template>
+
+<style scoped>
+.router-link-exact-active {
+  border-bottom: 1px solid white;
+}
+</style>
