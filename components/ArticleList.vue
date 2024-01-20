6da5fb77f9fcf5dd572e9f8806b547a9c61f2084
ThomasdevLC <thomas.lecam@gmail.com>
2024-01-20 15:12:36 +0100

    articleList component

---
+++ components/ArticleList.vue
@@ -0,0 +1,28 @@
+<!-- ArticleCard.vue -->
+<template>
+  <div class="w-full flex justify-around border-b-2 border-black py-8">
+    <div class="w-3/5">
+      <h2 class="font-title font-bold text-5xl mt-2">{{ article.title }}</h2>
+      <p class="mt-2">{{ article.content }}</p>
+      <p class="mt-2">
+        <span class="font-semibold">{{ article.author }} </span> | {{ article.date }}
+      </p>
+      <div class="align-bottom">
+        <NuxtLink :to="`/article/${article.id}`"
+          ><p class="mt-4 text-2xl">Lire l'article <span class="material-symbols-outlined"> north_east </span></p>
+        </NuxtLink>
+      </div>
+    </div>
+    <div class="h-80 w-96">
+      <img :src="'http://localhost:1337' + article.image.url" :alt="article.image.alternativeText" class="h-full w-full object-cover" />
+    </div>
+  </div>
+</template>
+
+<script setup>
+const props = defineProps(["article"]);
+</script>
+
+<style scoped>
+/* Add any scoped styles if needed */
+</style>
