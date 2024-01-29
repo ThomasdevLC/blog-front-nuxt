df133ff80b0a97df0ba20e1a88f18fceecab8735
ThomasdevLC <thomas.lecam@gmail.com>
2024-01-29 17:23:09 +0100

    nav style

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,7 @@
 <template>
   <header class="px-12 md:px-14 py-8 font-nunito font-bold">
     <div class="flex justify-between items-center">
-      <p class="text-2xl lg:text-3xl">Amitiés Taléa</p>
+      <p class="text-3xl lg:text-3xl">Amitiés Taléa</p>
 
       <button @click="toggleMenu" class="block md:hidden text-2xl">&#9776;</button>
 
@@ -12,7 +12,7 @@
         <NuxtLink to="/news">Actualités</NuxtLink>
       </div>
 
-      <div v-if="showMenu" class="md:hidden flex flex-col bg-white bg-opacity-80 absolute text-2xl px-10 py-5 space-y-3 border border-gray-800 top-20">
+      <div v-if="showMenu" class="md:hidden flex flex-col bg-white bg-opacity-90 absolute text-2xl px-10 py-5 space-y-3 border border-gray-800 top-20">
         <NuxtLink to="/">Accueil</NuxtLink>
         <NuxtLink to="/infos">Infos</NuxtLink>
         <NuxtLink to="/events">Événements</NuxtLink>
